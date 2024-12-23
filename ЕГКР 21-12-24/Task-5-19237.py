acbec50aa872268a8e599de6a0da100a284188b9
DHohlov <mr_dmitrii06@mail.ru>
2024-12-23 17:01:43 +0200

    https://kompege.ru/variant?kim=25073855
    Task 5 completed

---
+++ ЕГКР 21-12-24/Task-5-19237.py
@@ -0,0 +1,20 @@
+def convert(num):
+    res = ''
+    while num:
+        res += str(num % 3)
+        num //= 3
+    return res[::-1]
+ans = []
+for N in range(1, 100_000):
+    R = convert(N)
+    if N % 3 == 0:
+        R += R[-2:]
+    else:
+        summ = convert(sum(map(int, R)))
+        R += summ
+    R = int(R, 3)
+    if R > 220 and R % 2 == 0:
+        ans.append(R)
+print(min(ans))
+
+
