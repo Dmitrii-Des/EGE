9273fbd392e56eb3883907fd622bf356a6563c72
DHohlov <mr_dmitrii06@mail.ru>
2025-01-27 16:29:48 +0200

    https://kompege.ru/variant?kim=25078582 Task 15 completed

---
+++ 27-01-25/Task-15-9545.py
@@ -0,0 +1,14 @@
+def dell(x, d):
+    return x % d == 0
+
+def f(A):
+    for x in range(1, 1_000):
+        u = (dell(x, 10) and dell(x, 26) and (x >= 300)) <= (A <= x)
+        if not u:
+            return False
+    return True
+
+for A in range(1, 100_000)[::-1]:
+    if f(A):
+        print(A)
+        break
