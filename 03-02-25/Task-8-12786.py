d4df6608466180db9bac04b52a1bb3220d43297e
DHohlov <mr_dmitrii06@mail.ru>
2025-02-03 16:41:05 +0200

    https://kompege.ru/variant?kim=25080153 Task 8 completed

--- 03-02-25/Task-8-12786.py
+++ 03-02-25/Task-8-12786.py
@@ -3,7 +3,8 @@ from itertools import permutations
 alph = sorted('МАКАКА')
 cnt = 0
 
-for val in permutations(alph, 6):
-    if 'ММ' not in val and 'АА' not in val and 'КК' not in val:
+for val in set(permutations(alph)):
+    val = ''.join(val)
+    if 'АА' not in val and 'КК' not in val:
         cnt += 1
 print(cnt)
--- 03-02-25/Task-8-13252.py
+++ 03-02-25/Task-8-13252.py
@@ -1,9 +1,9 @@
 from itertools import permutations
 cnt = 0
-alph = sorted(set('КИДАЛА'))
+alph = sorted('КИДАЛА')
 
-for val in permutations(alph, 5):
+for val in set(permutations(alph, 5)):
     val = ''.join(val)
-    if 'АА' not in val and 'КК' not in val and 'ДД' not in val and 'ЛЛ' not in val:
+    if 'АА' not in val:
         cnt += 1
 print(cnt)
