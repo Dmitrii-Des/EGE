ef207a716db503a08a341e5da7b3b7a0d12dc206
DHohlov <mr_dmitrii06@mail.ru>
2025-02-20 17:32:48 +0200

    https://kompege.ru/variant?kim=25083847 Task 25 completed(method 2)

---
+++ 20-02-25/Task-25-11955(1).py
@@ -0,0 +1,8 @@
+from fnmatch import fnmatch
+
+for i in range(1021574 - 1021574 % 133, 10**10, 133):
+    if fnmatch(str(i), '1[02468]21574') or\
+            fnmatch(str(i), '1[02468]2157[13579]4') or\
+            fnmatch(str(i), '1[02468]2157[13579][13579]4') or\
+            fnmatch(str(i), '1[02468]2157[13579][13579][13579]4')
+        print(i, i//133)
