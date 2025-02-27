c9befbcbb323b1a941fa19964b12580b628043b3
DHohlov <mr_dmitrii06@mail.ru>
2025-02-27 16:25:28 +0200

    https://kompege.ru/variant?kim=25085293 Task 2 completed

---
+++ 27-02-25/Task-2-2205.py
@@ -0,0 +1,13 @@
+from itertools import product, permutations
+def f(H, L, W, N):
+    return (not(H <= L)) <= ((not(W <= N)) and H)
+
+for a1, a2, a3, a4, a5, a6 in product([0, 1], repeat=6):
+    table = [(0, 0, 0, a1), (0, 0, a2, a3), (0, a4, a5, a6)]
+    if len(table) == len(set(table)):
+        for p in permutations('HLWN'):
+            u = [f(**dict(zip(p, t))) for t in table] == [0, 0, 0]
+            if u:
+                print(*p)
+
+
