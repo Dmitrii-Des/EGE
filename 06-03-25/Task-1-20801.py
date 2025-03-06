fb968d0455a043d9c5d1dbe27519aab75e41da68
DHohlov <mr_dmitrii06@mail.ru>
2025-03-06 16:23:17 +0200

    https://kompege.ru/variant?kim=25086035 Task 1 completed

---
+++ 06-03-25/Task-1-20801.py
@@ -0,0 +1,10 @@
+from itertools import permutations
+
+graph = 'AC CG GB BD DE EA FG FC FE'.split()
+matrix = '26 147 456 237 37 13 245'.split()
+
+print(*range(1, 8))
+
+for i in permutations('ACGBDFE'):
+    if all(str(i.index(x) + 1) in matrix[i.index(y)] for x, y in graph):
+        print(*i)
