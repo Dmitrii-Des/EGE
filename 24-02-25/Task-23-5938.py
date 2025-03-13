8ddf568dbb07f018609ef1f449d89f0235112646
DHohlov <mr_dmitrii06@mail.ru>
2025-03-13 17:39:47 +0200

    https://kompege.ru/variant?kim=25088219 Task 23 completed

---
+++ 24-02-25/Task-23-5938.py
@@ -0,0 +1,6 @@
+def f(cur, end, cnt):
+    if cur == end and cnt == 51: return 1
+    if cur > end: return 0
+    return f(cur*4, end, cnt + 1) + f(cur+1, end, cnt + 1) + f(cur*3, end, cnt + 1)
+
+print(f(2, 404, 0))
