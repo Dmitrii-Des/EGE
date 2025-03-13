acfe535d7cb0e5c26d24547c589aad3d79db15b0
DHohlov <mr_dmitrii06@mail.ru>
2025-03-13 16:28:10 +0200

    https://kompege.ru/variant?kim=25088217 Task 2 completed

--- 13-03-25/Task-2-14651.py
+++ 13-03-25/Task-2-14651.py
@@ -2,7 +2,6 @@ from itertools import product, permutations
 
 def f(x, y, z, w):
     return ((z <= x) == ((w <= y) or (x and w)))
-
 for a1, a2, a3, a4, a5, a6 in product([0, 1], repeat=6):
     table = [(a1, a2, a3, 1), (a4, a5, 1, 1), (a6, 1, 1, 1)]
     if len(table) == len(set(table)):
