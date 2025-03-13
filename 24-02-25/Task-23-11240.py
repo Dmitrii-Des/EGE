4570d9c1941071728ddee252812e35f1d226e5cf
DHohlov <mr_dmitrii06@mail.ru>
2025-03-13 17:05:48 +0200

    https://kompege.ru/variant?kim=25088219 Task 23 completed

---
+++ 24-02-25/Task-23-11240.py
@@ -0,0 +1,9 @@
+def f(cur, end, B):
+    if cur == end: return 1
+    if cur > end: return 0
+    if B:
+        return f(cur + 2, end, False) +  f(cur * 3, end, False)
+    if B != 2:
+        return f(cur + 2, end, False) + f(cur**2, end, True) + f(cur*3, end, False)
+
+print(f(2, 64, False))
