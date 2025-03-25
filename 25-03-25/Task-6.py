481ddef3bdd4948a5640b121e55be2c63cdbf138
DHohlov <mr_dmitrii06@mail.ru>
2025-03-25 11:34:27 +0200

    https://kompege.ru/variant?kim=25090569 Task 6 completed

---
+++ 25-03-25/Task-6.py
@@ -0,0 +1,32 @@
+from turtle import*
+tracer(0)
+m = 10
+lt(90)
+for i in range(9):
+    fd(17*m)
+    lt(90)
+    fd(8*m)
+    lt(90)
+    fd(17*m)
+up()
+lt(90)
+fd(3*m)
+rt(90)
+fd(5*m)
+lt(90)
+down()
+for i in range(4):
+    fd(97*m)
+    rt(90)
+    fd(132*m)
+    rt(90)
+up()
+
+for x in range(-20, 20):
+    for y in range(-20, 20):
+        goto(x*m, y*m)
+        dot('3', 'white')
+
+print(12*2+5*2)
+done()
+update()
