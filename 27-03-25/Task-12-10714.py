2c6711cd2f9c14c9e1713b16362ca4c278ef6a37
DHohlov <mr_dmitrii06@mail.ru>
2025-03-27 16:15:34 +0200

    https://kompege.ru/variant?kim=25091042 Task 12 completed

---
+++ 27-03-25/Task-12-10714.py
@@ -0,0 +1,14 @@
+lenn = 0
+ans = []
+for n in range(4, 1_000):
+    st = '1' + '2' * n
+    while '12' in st or '322' in st or '222' in st:
+        st = st.replace('12', '2', 1)
+        st = st.replace('322', '21', 1)
+        st = st.replace('222', '3', 1)
+        dlina = len(st)
+        if dlina > lenn:
+            lenn = dlina
+
+print(lenn)
+
