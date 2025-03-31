01a8dcdce67d5888e52d1b1b6fbcdded3bc3cbd6
DHohlov <mr_dmitrii06@mail.ru>
2025-03-31 17:45:53 +0200

    https://kompege.ru/variant?kim=25090444 Task 19-21 completed

--- 31-03-25/Task-19-21-17532.py
+++ 31-03-25/Task-19-21-17532.py
@@ -3,7 +3,7 @@ def f(x, y, s):
     if s == 0:
         return False
     h = [f(x + 1, y, s - 1), f(x, y + 1, s - 1), f(x * 3, y, s - 1), f(x, y * 3, s - 1)]
-    return any(h) if (s - 1) % 2 == 0 else all(h)
+    return any(h) if (s - 1) % 2 == 0 else all(h) #Для 19 задача all заменить  на any
 
 print('19', [s for s in range(1, 59) if f(s,  6, 2)])
 print('20', [s for s in range(1, 59) if f(s,  6, 3) and not f(s, 6, 1)])
