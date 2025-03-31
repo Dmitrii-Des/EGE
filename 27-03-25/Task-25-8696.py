5d5bffe104ab8962b3973a659b54520b7a350940
DHohlov <mr_dmitrii06@mail.ru>
2025-03-31 16:58:00 +0200

    https://kompege.ru/task Task 25 completed

--- 27-03-25/Task-25-8696.py
+++ 27-03-25/Task-25-8696.py
@@ -1,6 +1,24 @@
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5)+1):
+        if num % i == 0:
+            return False
+    return True
+
 def f(num):
     divs = set()
     for i in range(2, int(num**0.5)+1):
         if num % i == 0:
-            divs |= {num, num//i}
-    if len(divs) < 2 == 0:
+            divs |= {i, num//i}
+    return sum(divs)
+
+cnt = 0
+
+for i in range(1_273_548, 10**10):
+    M = f(i)
+    if is_prime(M % 100_000):
+        print(i, M)
+        cnt += 1
+        if cnt == 5:
+            break
