acd91683ac027a82d76ddb48015c2908626a0c98
DHohlov <mr_dmitrii06@mail.ru>
2025-04-07 16:56:57 +0200

    https://kompege.ru/variant?kim=25063517 Task 9 completed

--- 07-04-25/Task-9-4614.py
+++ 07-04-25/Task-9-4614.py
@@ -2,16 +2,18 @@ def f1(nums):
     return max(nums) < sum(nums) - max(nums)
 
 def f2(nums):
-    if len(set(nums)) == 3:
-        return True
-    return False
+    return len(set(nums)) == 3
+
+def f3(nums):
+    cnt = [nums.count(i) for i in nums]
+    return cnt.count(2) == 2 and cnt.count(1) == 2
 with open('9_4614.txt') as file:
     data = [list(map(int, i.split())) for i in file]
 
 cnt = 0
 
 for i in data:
-    if f1(i) and f2(i):
+    if f1(i) and f2(i) and f3(i):   #Вторая функция f2 менее гибкая, третья функция f3. Лучше использовать f3
         cnt += 1
 
 print(cnt)
