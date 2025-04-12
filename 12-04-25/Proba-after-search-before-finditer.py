4b519bcdedde622604fe0e835a5aca6fba67a6c0
DHohlov <mr_dmitrii06@mail.ru>
2025-04-12 19:12:35 +0200

    https://future-step.ru/tutor/python-regexp/ Только search

--- 12-04-25/Proba-after-search-before-finditer.py
+++ 12-04-25/Proba-after-search-before-finditer.py
@@ -6,7 +6,6 @@ pattern = r'ЕГЭ'
 
 res = re.search(pattern, string)
 
-
 print(f'Найдена подстрока: {res.group()}')  #Как с match, но с search. Search ищет во всей строке,\
                                             # match - первое вхождение
 print(f'Начало вхождения: {res.start()}')
