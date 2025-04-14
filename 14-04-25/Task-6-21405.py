967b38c265c96dc6d922f7b057a8c8687a4f4a80
DHohlov <mr_dmitrii06@mail.ru>
2025-04-14 16:17:22 +0200

    https://kompege.ru/variant?kim=25093318 Task 6 completed

---
+++ 14-04-25/Task-6-21405.py
@@ -0,0 +1,19 @@
+from turtle import*
+m = 10
+tracer(0)
+lt(90)
+
+rt(30)
+for i in range(3):
+    rt(150)
+    fd(6 * m)
+    rt(30)
+    fd(12 * m)
+up()
+
+for x in range(-20, 20):
+    for y in range(-20, 20):
+        goto(x * m, y * m)
+        dot(3, 'magenta')
+update()
+done()
