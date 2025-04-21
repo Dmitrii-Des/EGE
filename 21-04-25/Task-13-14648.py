d6bf6604d18baff94c5e8f899f22b4b3433d03bf
DHohlov <mr_dmitrii06@mail.ru>
2025-04-21 17:14:23 +0200

    https://kompege.ru/variant?kim=25094484 Task 13 completed

---
+++ 21-04-25/Task-13-14648.py
@@ -0,0 +1,13 @@
+from ipaddress import ip_network, ip_address
+
+ip_net = ip_address('218.48.192.0')
+ip = ip_address('218.48.192.56')
+cnt = 0
+
+for mask in range(16, 25):
+    net = ip_network(f'{ip_net}/{mask}', False)
+    if net.num_addresses >= 502 and ip_net != net.broadcast_address and ip_net == net.network_address and\
+            ip in net.hosts():   #ip_net != net.broadcast_addres необязательно, адрес сети не может быть широковещателем
+        cnt += 1
+
+print(cnt)
